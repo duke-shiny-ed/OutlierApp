2c4efd2c6d377cab00b1b457d556bbb8266fd8ff
glenmorgenstern <glenmorgenstern@gmail.com>
2020-07-09 13:36:53 -0500

    Copied Shiny code from my repository to ShinyEd

--- OutlierApp/app.R
+++ OutlierApp/app.R
@@ -1,47 +1,108 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
+library(tidyverse)
+library(dplyr)
+library(ggthemes)
 
-# Define UI for application that draws a histogram
+initial <- read.csv("data/airqedited.csv")
+noHighIncome <- read.csv("data/airq-no-high-income.csv")
+noMediumIncome <- read.csv("data/airq-no-medium-income.csv")
+noOutliers <- read.csv("data/airq-no-outliers.csv")
+
+# Define UI for application
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+    titlePanel("How to Identify and Deal with Outliers"),
+    navbarPage("Tabs",
+               tabPanel("About",
+                        fluidRow(
+                            column(3,
+                                   h2("About")
+                            )
+                        ), # end of title row
+                        fluidRow(
+                            "In this app, you will learn methods for recognizing and treating outliers in your data.
+            These include:"
+                        ),
+                        fluidRow(
+                            column(6,
+                                   h5("Identifying Outliers \n"),
+                                   
+                            ),
+                            column(6,
+                                   h5("Dealing with Outliers"),
+                                   
+                            )
+                        ) #end of second row
+               ), #end of first tab
+               
+               tabPanel("Identify Outliers",
+                        
+                        # Sidebar with checkboxes
+                        sidebarLayout(
+                            sidebarPanel(
+                                helpText("On the right, you see a simple linear model relating median household income and value added by businesses. 
+                     Toggle the checkboxes below to remove some data points from the model."),
+                                checkboxGroupInput("remove",
+                                                   "Click to remove:",
+                                                   c("Middle Income Outliers" = "mid2",
+                                                     "High Income Outliers" = "high2")),
+                            ),
+                            # Show a plot of the generated model
+                            mainPanel(
+                                plotOutput("outlierGraph")
+                            )
+                        ) 
+               ), #end of second tab
+               
+               tabPanel("Measuring Outliers",
+                        sidebarLayout(
+                            sidebarPanel(
+                                helpText("Toggle the radio buttons to learn how to calculate measures for outliers and what they mean."),
+                                radioButtons("measure",
+                                             "Choose your measure:",
+                                             c("Cook's Distance" = "cooksDistance",
+                                               "Standardized residuals" = "standardizedResiduals",
+                                               "Leverage" = "leverage"
+                                             ))
+                            ),
+                            mainPanel(
+                                plotOutput("measureGraph"),
+                                textOutput("measureExplanation")
+                            )
+                        )
+               ), #end of third tab
+               
+               tabPanel("Solutions for Outliers",
+                        sidebarLayout(
+                            sidebarPanel(
+                                helpText("Help text here"),
+                                checkboxGroupInput("solution",
+                                                   "Click to perform the indicated action on the model:",
+                                                   c("Remove Middle Income Outlier" = "mid4",
+                                                     "Remove High Income Outliers" = "high4",
+                                                     "Log Transform the Data" = "logTransform"))
+                            ),
+                            mainPanel(
+                                plotOutput("solutionsGraph")
+                            )
+                        )
+               ), #end of fourth tab
+               tabPanel("Quiz",
+                        "Insert Quiz"
+               )
     )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw model
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$outlierGraph <- renderPlot({
+        ggplot(data=initial, aes(x=medi, y=vala)) + geom_point() + 
+            labs(title = "Business Value Added vs. Median Income",
+                 x = "Median Household Income", y = "Business Value Added") + 
+            geom_smooth(method = "lm", se = FALSE) + theme_economist()
+        
+        
     })
 }
 
